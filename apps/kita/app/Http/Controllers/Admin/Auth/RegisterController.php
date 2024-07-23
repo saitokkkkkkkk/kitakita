48a7f7149fff9b827409902d5113e9bb9eb67ebb
saitokkkkkkkk <k_saito@craid-inc.com>
2024-07-23 18:42:47 +0900

    Removed unused files

--- apps/kita/app/Http/Controllers/Admin/Auth/RegisterController.php
+++ apps/kita/app/Http/Controllers/Admin/Auth/RegisterController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
-use App\Models\User;
+use App\Models\Member;
 use Illuminate\Foundation\Auth\RegistersUsers;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
@@ -59,11 +59,11 @@ class RegisterController extends Controller
      * Create a new user instance after a valid registration.
      *
      * @param  array  $data
-     * @return \App\Models\User
+     * @return \App\Models\Member
      */
     protected function create(array $data)
     {
-        return User::create([
+        return Member::create([
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
