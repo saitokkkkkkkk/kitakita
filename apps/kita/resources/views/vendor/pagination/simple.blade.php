3a4f3654284245e519bbf653db831c531b4b42ed
saitokkkkkkkk <k_saito@craid-inc.com>
2024-08-01 16:14:16 +0900

    Changed pagination view

--- apps/kita/resources/views/vendor/pagination/simple.blade.php
+++ apps/kita/resources/views/vendor/pagination/simple.blade.php
@@ -1,6 +1,6 @@
 @if ($paginator->hasPages())
     <nav role="navigation" aria-label="Pagination Navigation" class="d-flex justify-content-center custom-pagination">
-        {{-- Previous Page Link --}}
+        {{-- 前ページのリンク --}}
         @if ($paginator->onFirstPage())
             <span class="relative inline-flex items-center px-4 py-2 text-sm font-medium text-green-600 bg-white border border-green-600 cursor-default leading-5 mx-1">
                 {!! __('Previous') !!}
@@ -11,31 +11,26 @@
             </a>
         @endif
 
-        {{-- Pagination Elements --}}
-        @foreach ($elements as $element)
-            {{-- Array Of Links --}}
-            @if (is_array($element))
-                @foreach ($element as $page => $url)
-                    {{-- Display Page Number --}}
-                    @if ($page == $paginator->currentPage())
-                        <span aria-current="page">
-                            <span class="relative inline-flex items-center text-sm font-medium text-white bg-green-600 border border-green-600 leading-5">{{ $page }}</span>
-                        </span>
-                    @else
-                        <a href="{{ $url }}" class="relative inline-flex items-center text-sm font-medium text-green-600 bg-white border border-green-600 leading-5 hover:text-green-500 focus:z-10 focus:outline-none focus:border-green-300 focus:shadow-outline-green active:bg-gray-100 active:text-gray-700 transition ease-in-out duration-150">
-                            {{ $page }}
-                        </a>
-                    @endif
-                @endforeach
-            @elseif ($element == '...')
-                {{-- Display Ellipsis --}}
-                <span class="relative inline-flex items-center px-4 py-2 text-sm font-medium text-green-600 bg-white border border-green-600 cursor-default leading-5 mx-1">
-                    {!! __('...') !!}
-                </span>
-            @endif
-        @endforeach
+        {{-- ページネーションの数字部分 --}}
+        @php
+            $start = max($paginator->currentPage() - 1, 1);
+            $end = min($paginator->currentPage() + 1, $paginator->lastPage());
+        @endphp
 
-        {{-- Next Page Link --}}
+        {{-- 数字を描画 --}}
+        @for ($page = $start; $page <= $end; $page++)
+            @if ($page == $paginator->currentPage())
+                <span aria-current="page">
+                    <span class="relative inline-flex items-center text-sm font-medium text-white bg-green-600 border border-green-600 leading-5">{{ $page }}</span>
+                </span>
+            @else
+                <a href="{{ $paginator->url($page) }}" class="relative inline-flex items-center text-sm font-medium text-green-600 bg-white border border-green-600 leading-5 hover:text-green-500 focus:z-10 focus:outline-none focus:border-green-300 focus:shadow-outline-green active:bg-gray-100 active:text-gray-700 transition ease-in-out duration-150">
+                    {{ $page }}
+                </a>
+            @endif
+        @endfor
+
+        {{-- 次ページのリンク --}}
         @if ($paginator->hasMorePages())
             <a href="{{ $paginator->nextPageUrl() }}" rel="next" class="relative inline-flex items-center px-4 py-2 text-sm font-medium text-green-600 bg-white border border-green-600 leading-5 hover:text-green-500 focus:z-10 focus:outline-none focus:border-green-300 focus:shadow-outline-green active:bg-gray-100 active:text-gray-700 transition ease-in-out duration-150 mx-1">
                 {!! __('Next') !!}
@@ -47,3 +42,4 @@
         @endif
     </nav>
 @endif
+
