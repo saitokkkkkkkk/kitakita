6d5b32aa36d20f77bbc7efc359ba9796b02acf37
saitokkkkkkkk <k_saito@craid-inc.com>
2024-08-04 20:54:57 +0900

    Use a magnifying glass icon if the width is narrower than 576px.

--- apps/kita/resources/views/layouts/app.blade.php
+++ apps/kita/resources/views/layouts/app.blade.php
@@ -9,35 +9,52 @@
                     <a class="navbar-brand bg-success fs-2 px-3 py-1 text-white text-decoration-none d-inline-flex justify-content-center align-items-center custom-ellipse" href="{{ route('articles.index') }}">
                         {{ config('app.name', 'Kita') }}
                     </a>
-                    <!-- 検索窓 -->
-                    {!! Form::open(['route' => 'articles.index', 'method' => 'GET', 'class' => 'd-flex align-items-center py-3']) !!}
-                        {!! Form::search('search', request('search'), ['class' => 'form-control form-control-sm border-success rounded-end-0', 'placeholder' => 'Search for something', 'aria-label' => '検索']) !!}
-                        {!! Form::button('検索', ['type' => 'submit', 'class' => 'btn btn-sm btn-success text-nowrap rounded-start-0 px-4 me-2 fs-6']) !!}
-                    {!! Form::close() !!}
-                    @auth
-                    <!-- 記事作成 -->
-                    <!-- 人マーク -->
+
+                    <!-- 576px以下になれば検索窓アイコンにする -->
                     <div class="dropdown">
-                        <a class="btn btn-outline-secondary dropdown-toggle" href="#" id="userDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                            <i class="far fa-user-circle"></i>
-                        </a>
-                        <ul class="dropdown-menu" aria-labelledby="userDropdown">
-                            <!-- ここに「プロフィール編集」 -->
-                            <li>
-                                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-inline">
-                                    @csrf
-                                    <button type="submit" class="dropdown-item">ログアウト</button>
-                                </form>
-                            </li>
-                        </ul>
+                        <button class="btn btn-outline-success  d-sm-none me-2" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            <i class="fas fa-search"></i>
+                        </button>
+                        <div class="dropdown-menu dropdown-menu-end p-2">
+                            {!! Form::open(['route' => 'articles.index', 'method' => 'GET', 'class' => 'd-flex align-items-center']) !!}
+                                {!! Form::search('search', request('search'), ['class' => 'form-control form-control-sm border-success rounded-end-0', 'placeholder' => 'Search for something']) !!}
+                                {!! Form::button('<i class="fas fa-search"></i>', ['type' => 'submit', 'class' => 'btn btn-sm btn-success rounded-start-0 px-1 me-2 fs-6']) !!}
+                            {!! Form::close() !!}
+                        </div>
                     </div>
+
+                    <!-- 検索フォーム -->
+                    <div id="search-form" class="d-none d-sm-flex align-items-center py-3">
+                        {!! Form::open(['route' => 'articles.index', 'method' => 'GET', 'class' => 'd-flex align-items-center']) !!}
+                            {!! Form::search('search', request('search'), ['class' => 'form-control form-control-sm border-success rounded-end-0', 'placeholder' => 'Search for something', 'aria-label' => '検索']) !!}
+                            {!! Form::button('検索', ['type' => 'submit', 'class' => 'btn btn-sm btn-success text-nowrap rounded-start-0 px-1 me-2 fs-6']) !!}
+                        {!! Form::close() !!}
+                    </div>
+
+                    <!-- 後でここに新規記事作成ボタン入れる -->
+
+                    <!-- 人マーク -->
+                    @auth
+                        <div class="dropdown">
+                            <a class="btn btn-outline-secondary dropdown-toggle" href="#" id="userDropdown" role="button" data-bs-toggle="dropdown">
+                                <i class="far fa-user-circle"></i>
+                            </a>
+                            <ul class="dropdown-menu dropdown-menu-end">
+                                <li>
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-inline">
+                                        @csrf
+                                        <button type="submit" class="dropdown-item">ログアウト</button>
+                                    </form>
+                                </li>
+                            </ul>
+                        </div>
                     @endauth
                 </div>
             </div>
         </div>
     </nav>
+
     <main>
         @yield('content')
     </main>
 </div>
-</body>
