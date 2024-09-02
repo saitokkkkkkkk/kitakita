fbe711ccac4d0ace1134999c3e23577081fb373d
saitokkkkkkkk <k_saito@craid-inc.com>
2024-09-02 16:26:37 +0900

    Added comment and phpdoc

--- apps/kita/app/Http/Controllers/Admin/AdminDeleteController.php
+++ apps/kita/app/Http/Controllers/Admin/AdminDeleteController.php
@@ -10,13 +10,26 @@ class AdminDeleteController extends Controller
 {
     protected $adminDeleteService;
 
+    /**
+     * Constructor for AdminDeleteController.
+     *
+     * @param AdminDeleteService $adminDeleteService
+     */
     public function __construct(AdminDeleteService $adminDeleteService)
     {
         $this->adminDeleteService = $adminDeleteService;
     }
 
+    /**
+     * Delete the admin.
+     *
+     * @param AdminUser $adminUser
+     * @return void これも後で変更
+     */
     public function destroy(AdminUser $adminUser)
     {
         $this->adminDeleteService->destroy($adminUser);
+
+        //あとでリダイレクト先を追加
     }
 }
